84cf3d14f88367b1afae1c4487f0a29715c2ef9e
YouthSaverAdmin <youthsaver12@gmail.com>
2024-09-28 18:09:42 +0800

    Add files via upload

---
+++ frontend/static/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show'); // Toggle the menu display
+});
