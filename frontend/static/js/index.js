6550ed8702ed9ea8fe799f441acb7d8658191233
YouthSaverAdmin <youthsaver12@gmail.com>
2024-09-28 18:13:06 +0800

    Delete frontend directory

--- frontend/static/js/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('show'); // Toggle the menu display
-});
